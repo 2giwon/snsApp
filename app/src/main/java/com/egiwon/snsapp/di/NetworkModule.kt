69bc0681415b272a66a822127817d04d5ad56cc1
middlefgr <2giwon@gmail.com>
2020-09-19 20:55:00 +0900

    #05 네트워크 모듈 추가

---
+++ app/src/main/java/com/egiwon/snsapp/di/NetworkModule.kt
@@ -0,0 +1,63 @@
+package com.egiwon.snsapp.di
+
+import com.egiwon.snsapp.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor().apply {
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideRxCallAdapterFactory() = RxJava2CallAdapterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory() = GsonConverterFactory.create()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        converterFactory: GsonConverterFactory,
+        rxCallAdapterFactory: RxJava2CallAdapterFactory,
+        client: OkHttpClient
+    ): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addCallAdapterFactory(rxCallAdapterFactory)
+            .addConverterFactory(converterFactory)
+            .build()
+
+
+    companion object {
+        private const val BASE_URL = "http://35.200.92.60:11000/"
+    }
+
+}
