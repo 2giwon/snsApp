8a91b7cf0e6647393564ad989e2fd28add823bb7
middlefgr <2giwon@gmail.com>
2020-09-20 10:20:02 +0900

    #10 remoteDataSource 추가

---
+++ app/src/main/java/com/egiwon/snsapp/data/source/ContentRemoteDataSource.kt
@@ -0,0 +1,23 @@
+package com.egiwon.snsapp.data.source
+
+import com.egiwon.snsapp.data.ContentDataSource
+import com.egiwon.snsapp.data.ContentService
+import com.egiwon.snsapp.data.entity.HomeContentResponse
+import io.reactivex.Single
+import javax.inject.Inject
+
+class ContentRemoteDataSource @Inject constructor(
+    private val contentService: ContentService
+) : ContentDataSource {
+
+    override fun getHomeContents(): Single<HomeContentResponse> =
+        contentService.getHomeContent()
+            .onErrorReturn {
+                HomeContentResponse(
+                    ok = false,
+                    popularCards = emptyList(),
+                    popularUsers = emptyList()
+                )
+            }
+
+}
